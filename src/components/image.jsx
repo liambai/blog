baefe32a47688d4393807c61fd1934bad2e602df
Liam <liambai2000@gmail.com>
2023-09-18 23:29:32 -0400

    Mutation effects

---
+++ src/components/image.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+const Image = ({ path, caption, width = 500, alt = "" }) => {
+  return (
+    <div style={{ margin: "auto", textAlign: "center" }}>
+      <img style={{ width: width }} src={path.default} alt={alt} />
+      {caption && (
+        <div
+          style={{
+            fontSize: 14,
+            marginTop: 10,
+            color: "slategrey",
+            textAlign: "left",
+          }}
+        >
+          {caption}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Image
