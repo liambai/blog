43e667facbc7a7344da58552355d65599bbb4074
Liam <liambai2000@gmail.com>
2023-09-19 15:07:48 -0400

    Fixes

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const Image = ({ path, caption, width = 500, alt = "" }) => {
   return (
-    <div style={{ margin: "auto", textAlign: "center" }}>
+    <div style={{ margin: "auto", textAlign: "center", marginBottom: 30 }}>
       <img style={{ width: width }} src={path.default} alt={alt} />
       {caption && (
         <div
