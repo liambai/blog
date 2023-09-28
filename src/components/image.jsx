3a136333ac17d71e17a8960522033cd36b4706d1
Liam <liambai2000@gmail.com>
2023-09-28 17:22:22 -0400

    Minor updates

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -2,12 +2,12 @@ import React from "react"
 
 import { useMediaQuery } from "react-responsive"
 
-const Image = ({ path, width = "100%", alt = "" }) => {
+const Image = ({ path, width = "100%", alt = "", style = {} }) => {
   const isMobile = useMediaQuery({ query: "(max-width: 1224px)" })
   // Width should always be 100% on mobile
   return (
     <img
-      style={{ width: isMobile ? "100%" : width, margin: "auto" }}
+      style={{ width: isMobile ? "100%" : width, margin: "auto", ...style }}
       src={path.default}
       alt={alt}
     />
