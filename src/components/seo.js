b26893d0ceb9d62998eeec25c0ee7156811cda90
Liam Bai <lbai@ginkgobioworks.com>
2024-01-20 11:44:11 -0500

    Use large image for preview

--- src/components/seo.js
+++ src/components/seo.js
@@ -35,6 +35,7 @@ const Seo = ({ description, title, children }) => {
       <meta property="og:title" content={title} />
       <meta property="og:description" content={metaDescription} />
       <meta property="og:type" content="website" />
+      <meta name="twitter:card" content="summary_large_image" />
       <meta
         name="twitter:creator"
         content={site.siteMetadata?.social?.twitter || ``}
