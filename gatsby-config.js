da2aed8d18ba3c6e5615bae0534e44cdd3d5f7a6
Liam <liambai2000@gmail.com>
2025-05-19 21:38:16 -0400

    Simplify

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,13 +36,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/blog/logit-lens/data`,
-        name: `logit-lens-data`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
