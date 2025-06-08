c568fd4af1fd9c4535b0c0e2da06d8aff5eb1a57
Liam <liambai2000@gmail.com>
2025-06-08 18:12:37 -0400

    Edits

--- content/blog/minimum-description-length/Notes.jsx
+++ content/blog/minimum-description-length/Notes.jsx
@@ -1,8 +1,10 @@
 import getNoteComponents from "../../../src/components/notes"
 
 const notes = {
-  1: "As you can probably guess, finding the minimum program is a tough problem in more practical situations. In fact, [Kolmogorov complexity is generally not computable](https://en.wikipedia.org/wiki/Kolmogorov_complexity#Uncomputability_of_Kolmogorov_complexity).",
-  2: "For programs like gzip, the $L(H)$ term is the length of the program in C, which is negligible compared to an LLM's weights.",
+  1: "As you can probably guess, finding the minimum program is an intractable problem in more practical situations. In fact, [Kolmogorov complexity is generally not computable](https://en.wikipedia.org/wiki/Kolmogorov_complexity#Uncomputability_of_Kolmogorov_complexity).",
+  2: "Technically, we need an infinite number of bits because real numbers have an infinite decimal expansion. Practically, we approximate to some finite decimal point.",
+  3: "In practice, we need an encoding algorithm (e.g. [Huffman coding](https://en.wikipedia.org/wiki/Huffman_coding) or [Arithmetic coding](https://en.wikipedia.org/wiki/Arithmetic_coding)). The codes produced by these methods are bounded by, and don't usually achieve, the theoretical optimal length $-log_2(p(x))$.",
+  4: "For classic algorithms like gzip, the $L(H)$ term is the length of program in its programming language, which is at most a few KBs, negligible compared to an LLM's weights.",
 }
 
 export const { Note, NoteList } = getNoteComponents(notes)
