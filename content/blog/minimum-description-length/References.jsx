4efdcd4fe72089f2fdd54f3c9e3a239f48a4feeb
Liam Bai <liambai2000@gmail.com>
2025-06-08 18:20:35 -0400

    MDL (#12)
    * MDL
    
    * Flesh out points
    
    * Edits
    
    * edits
    
    * References
    
    * edits
    
    * Words
    
    * Conclusion
    
    * Edits
    
    * Fixes
    
    * Note
    
    * Revamp notes to support markdown
    
    * illustrations
    
    * edit
    
    * Edits
    
    * edits

---
+++ content/blog/minimum-description-length/References.jsx
@@ -0,0 +1,45 @@
+import getReferenceComponents from "../../../src/components/references"
+
+const references = {
+  1: {
+    title: "Kolmogorov Complexity and Algorithmic Randomness",
+    author: "Shen, A., Uspensky, V. A., & Vereshchagin, N.",
+    url: "https://www.lirmm.fr/~ashen/kolmbook-eng-scan.pdf",
+    journal: "Mathematical Surveys and Monographs",
+    year: 2017,
+  },
+  2: {
+    title:
+      "A tutorial introduction to the minimum description length principle",
+    author: "Grünwald, P.",
+    url: "https://arxiv.org/abs/math/0406077",
+    journal: "arXiv",
+    year: 2004,
+  },
+  3: {
+    title:
+      "Keeping the neural networks simple by minimizing the description length of the weights",
+    author: "Hinton, G. E., & van Camp, D.",
+    url: "https://dl.acm.org/doi/10.1145/168304.168306",
+    journal:
+      "Proceedings of the sixth annual conference on Computational learning theory",
+    year: 1993,
+  },
+  4: {
+    title: "The First Law of Complexodynamics",
+    author: "Aaronson, S.",
+    url: "https://scottaaronson.blog/?p=762",
+    journal: "Shtetl-Optimized",
+    year: 2011,
+  },
+  5: {
+    title: "Language Modeling Is Compression",
+    author:
+      "Delétang, G., Ruoss, A., Duquenne, P. A., Catt, E., Genewein, T., Mattern, C., Grau-Moya, J., Wenliang, L. K., Aitchison, M., Orseau, L., Hutter, M., & Veness, J.",
+    url: "https://arxiv.org/abs/2309.10668",
+    journal: "arXiv",
+    year: 2024,
+  },
+}
+
+export const { Reference, ReferenceList } = getReferenceComponents(references)
