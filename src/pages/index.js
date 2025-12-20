182cd195abd2cb4def12799687135631cb98054a
Liam <liambai2000@gmail.com>
2025-12-20 00:27:19 -0500

    Fix links

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,31 +21,53 @@ const IndexPage = ({ data, location }) => {
       <div className="landing-simple">
         <p className="landing-summary">
           Hi! I'm Liam. I work on software at{" "}
-          <a href="https://generatebiomedicines.com/">Generate:Biomedicines</a>.
+          <a
+            href="https://generatebiomedicines.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Generate:Biomedicines
+          </a>
+          .
         </p>
         <p className="landing-summary">
           I am interested in protein design, interpretability, and systems for
-          automating scientific discovery. I enjoy writing about things I learn.
+          automating scientific discovery. I enjoy writing about cool things I
+          learn.
         </p>
         <Link to="/blog" className="blog-cta-button">
           My blog <FaArrowRight className="arrow-icon" />
         </Link>
         <div className="landing-links">
-          <a href={`https://www.x.com/${social.twitter}`} aria-label="Twitter">
+          <a
+            href={`https://www.x.com/${social.twitter}`}
+            aria-label="Twitter"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaTwitter />
           </a>
-          <a href="https://github.com/liambai" aria-label="GitHub">
+          <a
+            href="https://github.com/liambai"
+            aria-label="GitHub"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <FaGithub />
           </a>
           <a
             href="https://scholar.google.com/citations?user=qBKzB2sAAAAJ&hl=en"
             aria-label="Google Scholar"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             <FaGraduationCap />
           </a>
           <a
             href={`https://www.linkedin.com/in/${social.linkedin}`}
             aria-label="LinkedIn"
+            target="_blank"
+            rel="noopener noreferrer"
           >
             <FaLinkedin />
           </a>
@@ -58,25 +80,46 @@ const IndexPage = ({ data, location }) => {
                   <InterProtPreview />
                 </div>
                 <div className="work-details">
-                  <a href="https://interprot.com/" className="work-title">
+                  <a
+                    href="https://interprot.com/"
+                    className="work-title"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     From Mechanistic Interpretability to Mechanistic Biology:
                     Training, Evaluating, and Interpreting Sparse Autoencoders
                     on Protein Language Models
                   </a>
                   <p className="work-authors">
-                    Etowah Adams*, Liam Bai*, Minji Lee, Yiyang Yu, Mohammed
-                    AlQuraishi
+                    Etowah Adams*, <u>Liam Bai*</u>, Minji Lee, Yiyang Yu,
+                    Mohammed AlQuraishi
                   </p>
                   <p className="work-venue">
                     International Conference on Machine Learning (ICML), 2025
                     (Spotlight)
                   </p>
                   <div className="work-links">
-                    <a href="https://interprot.com/">Website</a>
-                    <a href="https://www.biorxiv.org/content/10.1101/2025.02.06.636901v2">
+                    <a
+                      href="https://interprot.com/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Website
+                    </a>
+                    <a
+                      href="https://www.biorxiv.org/content/10.1101/2025.02.06.636901v2"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
                       Paper
                     </a>
-                    <a href="https://github.com/etowahadams/interprot">Code</a>
+                    <a
+                      href="https://github.com/etowahadams/interprot"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Code
+                    </a>
                   </div>
                 </div>
               </div>
