bd426f1640a14e4efb37c7029c0d91aabfef0be5
Liam <liambai2000@gmail.com>
2026-08-02 01:07:22 -0400

    Add

--- content/blog/llm-jailbreak/References.jsx
+++ content/blog/llm-jailbreak/References.jsx
@@ -9,20 +9,27 @@ const references = {
     year: 2024,
   },
   2: {
+    title: "Improving Language Understanding by Generative Pre-Training",
+    author: "Radford, A. et al.",
+    url: "https://cdn.openai.com/research-covers/language-unsupervised/language_understanding_paper.pdf",
+    journal: "OpenAI",
+    year: 2018,
+  },
+  3: {
     title: "A General Language Assistant as a Laboratory for Alignment",
     author: "Askell, A. et al.",
     url: "https://arxiv.org/abs/2112.00861",
     journal: "arXiv",
     year: 2021,
   },
-  3: {
+  4: {
     title: "Jailbroken: How Does LLM Safety Training Fail?",
     author: "Wei, A. et al.",
     url: "https://arxiv.org/pdf/2307.02483",
     journal: "NeurIPS",
     year: 2023,
   },
-  4: {
+  5: {
     title: "Many-shot Jailbreaking",
     author: "Anil, C. et al.",
     url: "https://www.anthropic.com/research/many-shot-jailbreaking",
